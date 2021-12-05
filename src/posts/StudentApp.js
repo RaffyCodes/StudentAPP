9192f93af7426e03a5dc9e13a703f4b1f159bb60
“Raffy” <Rafaeamarrero@gmail.com>
2021-12-04 19:25:54 -0600

    Initial Commit

---
+++ src/posts/StudentApp.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../studentstyle.css'
+import StudentDetail from './StudentDetail'
+
+
+
+
+
+
+function StudentApp () {
+  
+  
+return (
+ 
+  <div className="wrapper">
+  <StudentDetail />
+         
+</div>
+)}
+         
+             
+export default StudentApp 
